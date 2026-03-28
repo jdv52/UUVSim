400145729cb770a84fd5e12025cfa4882e662c01
jdv52 <jaysondv0928@gmail.com>
2026-03-28 04:16:47 -0400

    Added some convenience scripts

---
+++ update_to_git.bat
@@ -0,0 +1,3 @@
+robocopy /MIR Plugins\AirSim ..\..\UNav-Sim\Unreal\Plugins\AirSim /XD temp Intermediate Binaries Saved *. /njh /njs /ndl /np
+robocopy /MIR Plugins\AirSim\Source\AirLib ..\..\UNav-Sim\AirLib /XD temp Intermediate Binaries Saved *. /njh /njs /ndl /np
+pause
